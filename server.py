d6e41994b52ae3e00f6819f1808ee347ba4d24df
Lee DongYun <80240079+dyoon00@users.noreply.github.com>
2026-06-15 13:06:24 +0900

    Initialize Flask server

---
+++ server.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    print("STM32 수신:", data)
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+```
